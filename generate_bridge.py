0aa917bfdc771c7a9412f696763d30d905210a1e
Luka140 <luka-groot@hotmail.nl>
2024-05-16 17:45:26 +0200

    Get displacements and reaction forces for the spaghet bridge

--- generate_bridge.py
+++ generate_bridge.py
@@ -1,100 +1,109 @@
 import numpy as np
+import os
+import pathlib
 import matplotlib.pyplot as plt
 from scipy.optimize import bisect, fsolve
 
-a1 = 2
-N = 10
-tanWidth = 4
-radWidth = 1
-midpoint = [0,-0.05]
-plotting = True
-spaghetti_area = 1
-spaghetti_diameter = np.sqrt(4*spaghetti_area/np.pi)
 
-span = 0.25
-b1 = a1*span**2
 
-# define parabola
+def generate_bridge(a1, N, tanWidth, radWidth, midpoint, plotting=False):
+    # a1 = 2
+    # N = 10
+    # tanWidth = 4
+    # radWidth = 1
+    # midpoint = [0,-0.05]
+    spaghetti_area = 1 #TODO adjust
+    spaghetti_diameter = np.sqrt(4*spaghetti_area/np.pi)
 
-def f(x):
-    return -a1*x**2 + b1
+    span = 0.25
+    b1 = a1*span**2
 
-def momentOfInertia(Width):
-    inertia = 0
-    # even case
-    if Width%2 == 0:
+    # define parabola
+
+    def f(x):
+        return -a1*x**2 + b1
+
+    def momentOfInertia(Width):
+        inertia = 0
+        # even case
+        if Width%2 == 0:
+            for i in range(int(Width/2)):
+                inertia += 2*Width * (np.pi/64 * spaghetti_diameter**4 + ((i+1)*spaghetti_diameter  - spaghetti_diameter/2)**2 * spaghetti_area)
+            # print(1/12 * (Width*spaghetti_diameter)**4)
+            # print(inertia)
+            return inertia
+
+        # odd case
+        inertia = Width * (np.pi/64 * spaghetti_diameter**4)
         for i in range(int(Width/2)):
-            inertia += 2*Width * (np.pi/64 * spaghetti_diameter**4 + ((i+1)*spaghetti_diameter  - spaghetti_diameter/2)**2 * spaghetti_area)
-        print(1/12 * (Width*spaghetti_diameter)**4)
-        print(inertia)
+            inertia += 2*Width * (np.pi/64 * spaghetti_diameter**4 + ((i+1)*spaghetti_diameter)**2 * spaghetti_area)
+        # print(1/12 * (Width*spaghetti_diameter)**4)
+        # print(inertia)
         return inertia
-    
-    # odd case
-    inertia = Width * (np.pi/64 * spaghetti_diameter**4)
-    for i in range(int(Width/2)):
-        inertia += 2*Width * (np.pi/64 * spaghetti_diameter**4 + ((i+1)*spaghetti_diameter)**2 * spaghetti_area)
-    print(1/12 * (Width*spaghetti_diameter)**4)
-    print(inertia)
-    return inertia
 
-def trasform(theta):
-    return np.array([[np.cos(theta), -np.sin(theta), 0 ,0]
-                     [np.sin(theta), np.cos(theta), 0, 0 ],
-                     [0, 0, np.cos(theta), -np.sin(theta)],
-                     [0, 0, np.sin(theta), np.cos(theta) ]])
+    # def trasform(theta):
+    #     return np.array([[np.cos(theta), -np.sin(theta), 0 ,0]
+    #                      [np.sin(theta), np.cos(theta), 0, 0 ],
+    #                      [0, 0, np.cos(theta), -np.sin(theta)],
+    #                      [0, 0, np.sin(theta), np.cos(theta) ]])
 
-x = np.linspace(0, 0.25, 20)
-y = f(x)
+    x = np.linspace(0, 0.25, 20)
+    y = f(x)
 
 
-alpha_start = np.arctan(-midpoint[1]/span)
-alphas = np.linspace(alpha_start, np.pi/2, N)
-nodes = np.zeros((N, 2))
+    alpha_start = np.arctan(-midpoint[1]/span)
+    alphas = np.linspace(alpha_start, np.pi/2, N)
+    nodes = np.zeros((N, 2))
 
-for i, alpha in enumerate(alphas):
-    if np.abs(alpha - np.pi/2) < 1e-6:
-        nodes[i, 0] = 0
-        nodes[i, 1] = f(0)
+    for i, alpha in enumerate(alphas):
+        if np.abs(alpha - np.pi/2) < 1e-6:
+            nodes[i, 0] = 0
+            nodes[i, 1] = f(0)
+            plt.plot([midpoint[0], nodes[i,0]], [midpoint[1], nodes[i,1]], 'r')
+            continue
+        y1 = lambda x: np.tan(alpha) * x + midpoint[1]
+        nodes[i, 0] = fsolve(lambda x: y1(x) - f(x), 3)
+        nodes[i, 1] = f(nodes[i, 0])
         plt.plot([midpoint[0], nodes[i,0]], [midpoint[1], nodes[i,1]], 'r')
-        continue
-    y1 = lambda x: np.tan(alpha) * x + midpoint[1]
-    nodes[i, 0] = fsolve(lambda x: y1(x) - f(x), 3)
-    nodes[i, 1] = f(nodes[i, 0])
-    plt.plot([midpoint[0], nodes[i,0]], [midpoint[1], nodes[i,1]], 'r')
 
-for i in range(len(nodes)-1):
-    plt.text(nodes[i,0]+ 1e-4, nodes[i,1]+ 1e-4, str(i+1))
-    plt.plot([nodes[i,0], nodes[i+1,0]], [nodes[i,1], nodes[i+1,1]], color='black')
-    if i == len(nodes)-2:
-        plt.text(nodes[i+1,0]+ 1e-4, nodes[i+1,1]+ 1e-4, str(i+2))
+    for i in range(len(nodes)-1):
+        plt.text(nodes[i,0]+ 1e-4, nodes[i,1]+ 1e-4, str(i+1))
+        plt.plot([nodes[i,0], nodes[i+1,0]], [nodes[i,1], nodes[i+1,1]], color='black')
+        if i == len(nodes)-2:
+            plt.text(nodes[i+1,0]+ 1e-4, nodes[i+1,1]+ 1e-4, str(i+2))
 
-# Connectivity matrix
-radial = np.vstack((np.zeros(N), np.arange(1, N+1))).T
-tangential = np.vstack((np.arange(1, N), np.arange(2, N+1))).T
-C = np.vstack((radial, tangential))
+    # Connectivity matrix
+    radial = np.vstack((np.zeros(N, dtype=np.integer), np.arange(1, N+1, dtype=np.integer))).T
+    tangential = np.vstack((np.arange(1, N,dtype=np.integer), np.arange(2, N+1, dtype=np.integer))).T
+    connection_matrix = np.vstack((radial, tangential))
 
-# Element data 
-idx = np.arange(0, C.shape[0])
+    # Element data
+    idx = np.arange(0, connection_matrix.shape[0])
 
-radial_lengths = np.sqrt((nodes[:, 0] - midpoint[0])**2 + (nodes[:, 1] - midpoint[1])**2)
-radial_areas = radWidth*radWidth*spaghetti_area * np.ones(N)
-radial_inertia = np.ones(N)*momentOfInertia(radWidth)
+    radial_lengths = np.sqrt((nodes[:, 0] - midpoint[0])**2 + (nodes[:, 1] - midpoint[1])**2)
+    radial_areas = radWidth*radWidth*spaghetti_area * np.ones(N)
+    radial_inertia = np.ones(N)*momentOfInertia(radWidth)
 
-tangential_lengths = np.sqrt((nodes[1:, 0] - nodes[:-1, 0])**2 + (nodes[1:, 1] - nodes[:-1, 1])**2)
-tangential_areas = tanWidth*tanWidth*spaghetti_area * np.ones(N-1)
-tangential_inertia = np.ones(N-1)*momentOfInertia(tanWidth)
+    tangential_lengths = np.sqrt((nodes[1:, 0] - nodes[:-1, 0])**2 + (nodes[1:, 1] - nodes[:-1, 1])**2)
+    tangential_areas = tanWidth*tanWidth*spaghetti_area * np.ones(N-1)
+    tangential_inertia = np.ones(N-1)*momentOfInertia(tanWidth)
 
-lengths = np.hstack((radial_lengths, tangential_lengths))  
-areas = np.hstack((radial_areas, tangential_areas))
-inertia = np.hstack((radial_inertia, tangential_inertia))
+    lengths = np.hstack((radial_lengths, tangential_lengths))
+    areas = np.hstack((radial_areas, tangential_areas))
+    inertia = np.hstack((radial_inertia, tangential_inertia))
 
-elements = np.vstack((idx, lengths, areas, inertia)).T
-nodes = np.vstack((midpoint,nodes))
-print(nodes)
-print(elements)
+    elements = np.vstack((idx, lengths, areas, inertia)).T
+    nodes = np.vstack((midpoint, nodes))
+    # print(nodes)
+    # print(elements)
 
-if plotting:
-    plt.text(midpoint[0], midpoint[1]+ 1e-4, str(0))
-    plt.scatter(nodes[:, 0], nodes[:, 1], c='g')
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.show()
+    if plotting:
+        plt.text(midpoint[0], midpoint[1]+ 1e-4, str(0))
+        plt.scatter(nodes[:, 0], nodes[:, 1], c='g')
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.show()
+
+    return nodes, elements, connection_matrix
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from generate_bridge import generate_bridge
 
 def StiffMat(Area, I, E, leng):  # define stiffness matrix
 
@@ -47,18 +47,23 @@ def StiffMat(Area, I, E, leng):  # define stiffness matrix
     return Ktot
 
 
-def assemble_K(E, areas, lengths, moi, connections):
-    n_nodes = lengths.size + 1
+def assemble_K(E, areas, lengths, moi, connections, node_positions):
+    n_nodes = node_positions.shape[0]
     K_global = np.zeros((n_nodes * 3, n_nodes * 3))
     for el_idx in range(len(connections)):
-        n1idx, n2idx = connections[el_idx, 0] - 1, connections[el_idx, 1] - 1
-        # n1, n2 = NodePos[n1idx,:], NodePos[n2idx,:]
+        n1idx, n2idx = connections[el_idx, 0], connections[el_idx, 1]
+        n1, n2 = node_positions[n1idx, :], node_positions[n2idx, :]
+        dir_vec = n2 - n1
+        theta = np.arctan2(dir_vec[1], dir_vec[0])
+
         K_local = StiffMat(areas[el_idx], moi[el_idx], E, lengths[el_idx])
-        # TODO here transform
+
+        T_mat = get_transform_mat(theta)
+        K_transformed = T_mat @ K_local @ T_mat.T
 
         indices = (3 * n1idx, 3 * n1idx + 1, 3 * n1idx + 2,
                    3 * n2idx, 3 * n2idx + 1, 3 * n2idx + 2)
-        K_global[np.ix_(indices, indices)] += K_local
+        K_global[np.ix_(indices, indices)] += K_transformed
     return K_global
 
 
@@ -66,58 +71,84 @@ def reduce_matrices(K, F, BC):
     K_reduced = np.copy(K)
     F_reduced = np.copy(F)
     for col_idx in range(BC.shape[1]):
-        dof_idx = BC[0, col_idx] - 1
+        dof_idx = BC[0, col_idx]
         bc_disp = BC[1, col_idx]
+
         F_reduced = F_reduced - K_reduced[:, dof_idx] * bc_disp
     # open_dof = np.ones(K.shape, dtype=bool)
     # open_dof[np.ix_(BC[0,:] -1, BC[0,:] -1)] = False
-    return np.delete(np.delete(K_reduced, BC[0, :] - 1, 0), BC[0, :] - 1, 1), \
-        np.delete(F_reduced, BC[0, :] - 1)
+    return np.delete(np.delete(K_reduced, BC[0, :], 0), BC[0, :], 1), \
+        np.delete(F_reduced, BC[0, :])
 
 
-def gen_con_matrix(element_count):
-    con_mat = np.matrix([[i, i + 1] for i in range(1, element_count + 1)])
-    return con_mat
+def get_transform_mat(theta):
+    return np.array([[np.cos(theta), -np.sin(theta), 0, 0, 0 , 0],
+                     [np.sin(theta), np.cos(theta), 0, 0, 0, 0],
+                     [0, 0, 1, 0, 0, 0],
+                     [0, 0, 0, np.cos(theta), -np.sin(theta), 0],
+                     [0, 0, 0, np.sin(theta), np.cos(theta), 0],
+                     [0, 0, 0, 0, 0, 1]])
 
-def gen_force_vector():
-    raise NotImplementedError
+# def numerical_solution(u_el, l_el, x):
+#     N1_T = (l_el ** 3 - 3 * l_el * x ** 2 + 2 * x ** 3) / l_el ** 3
+#     N2_T = (l_el ** 2 * x - 2 * x ** 2 * l_el + x ** 3) / l_el ** 2
+#     N3_T = (3 * x ** 2 * l_el - 2 * x ** 3) / l_el ** 3
+#     N4_T = (x ** 3 - x ** 2 * l_el) / l_el ** 2
+#     v = N1_T * u_el[1] + N2_T * u_el[2] + N3_T * u_el[4] + N4_T * u_el[5]
+#
+#     N1_A = 1 - x / l_el
+#     N2_A = x / l_el
+#     u = N1_A * u_el[0] + N2_A * u_Fel[3]
+#     return u, v
 
 
-def numerical_solution(u_el, l_el, x):
-    N1_T = (l_el ** 3 - 3 * l_el * x ** 2 + 2 * x ** 3) / l_el ** 3
-    N2_T = (l_el ** 2 * x - 2 * x ** 2 * l_el + x ** 3) / l_el ** 2
-    N3_T = (3 * x ** 2 * l_el - 2 * x ** 3) / l_el ** 3
-    N4_T = (x ** 3 - x ** 2 * l_el) / l_el ** 2
-    v = N1_T * u_el[1] + N2_T * u_el[2] + N3_T * u_el[4] + N4_T * u_el[5]
-
-    N1_A = 1 - x / l_el
-    N2_A = x / l_el
-    u = N1_A * u_el[0] + N2_A * u_el[3]
-    return u, v
-
-
-def simulate_frame(E, n_elem, a, b, qx, w_frame, h_frame, l_frame, BC):
-    con = gen_con_matrix(n_elem)
-    lengths = np.array([l_frame / n_elem for i in range(n_elem)])
-    moi = np.array([w_frame * h_frame ** 3 / 12 for i in range(n_elem)])
-    areas = np.array([h_frame * w_frame for i in range(n_elem)])
-
-    K = assemble_K(E, areas, lengths, moi, con)
-    force = gen_force_vector()
-    K_red, F_red = reduce_matrices(K, force, BC)
+def simulate_frame(E, node_pos, elements, connections, BC, forces):
+    K = assemble_K(E, areas=elements[:,2], lengths=elements[:,1], moi=elements[:,3], connections=connections, node_positions=node_pos)
+    K_red, F_red = reduce_matrices(K, forces, BC)
     u_global = np.linalg.solve(K_red, F_red)
 
     #  make this less disgusting later, this is not very robust
-    u = np.zeros((n_elem + 1) * 3)
+    u = np.zeros((node_pos.shape[0]) * 3)
     u[BC[0, :] - 1] = BC[1, :]
-    u[[idx for idx in range((n_elem + 1) * 3) if idx + 1 not in BC[0, :]]] = u_global
+    u[[idx for idx in range((node_pos.shape[0]) * 3) if idx not in BC[0, :]]] = u_global
 
-    reactions = K @ u - force
+    reactions = K @ u - forces
 
     return u, reactions
 
 
-if __name__=="__main__":
-    BC = np.array([[1, 2, 3], [0, 0, 0]])
 
-    simulate_frame(10, 3, 5,5,5,5,5,5,BC)
+
+
+if __name__=="__main__":
+    disp_vis_scale = 10
+    elasticity_modulus = 10e6
+    plotting = True
+    node_pos, element_data, connection_matrix = generate_bridge(a1=2, N=10, tanWidth=4, radWidth=1, midpoint=[0, -0.05])
+
+    # FORMAT OF BCS IS: first row is the degrees of freedom, second row is the constrained displacements
+    sym_constraint_idx_top = [(node_pos.shape[0] - 1) * 3, (node_pos.shape[0] - 1) * 3 + 2]
+    # Index 0: horizontal symmetry for the central node
+    # indices 3,4,5 are a clamp at the rightmost bridge point
+    # sym_constraint_idx_top is the symmetry constraint for the midpoint of the arc
+    BC_idx =  [0, 3, 4, 5] + sym_constraint_idx_top
+    BC_disp = [0, 0, 0, 0, 0, 0]
+    BC = np.array([BC_idx, BC_disp])
+
+    mass = 1000000  # kg
+    force_vector = np.zeros(node_pos.shape[0]*3)
+    force_vector[1] = - mass * 9.81 / 2
+
+    displacements, reactions = simulate_frame(elasticity_modulus, node_pos, element_data, connection_matrix, BC, force_vector)
+
+
+
+
+    if plotting:
+        disp = displacements.reshape(-1, 3)
+        new_pos_viz = node_pos + disp[:, :2] * disp_vis_scale
+        plt.scatter(node_pos[:, 0], node_pos[:, 1], c='g')
+        plt.scatter(new_pos_viz[:, 0], new_pos_viz[:, 1], c='b')
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.show()
+    # TODO GET STRESSES
